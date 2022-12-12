f842d1486ffff4869be6e440f5e736105d1346c4
ivan_winLap <ivan.ea7h@gmail.com>
2022-12-12 22:38:20 +0100

    paths of resources/versions from json

--- java_CI_scripts/src/collection.clj
+++ java_CI_scripts/src/collection.clj
@@ -2,19 +2,37 @@
   (:require [babashka.fs :as fs]
             [cheshire.core :as json]))
 
-(comment
-  (json/parse-string (slurp (fs/file "pending_matrix" "two_models.json")) true)
-  )
-;(slurp "abc")
-(defn json->vector
-  "Returns the parsed list of resources/versions to test given a string json"
+(def COLLECTION-ROOT (fs/path ".." "bioimageio-gh-pages" "rdfs"))
+(defn str-json->vector
+  "Returns the parsed list of resources/versions to test given a raw json string"
   [str-json]
   (let [parsed (json/parse-string str-json true)
         {the-list :include} parsed]
     the-list))
+(defn file-json->vector
+  "Returns the parsed list of resources/versions to test given a json file"
+  [file-json]
+  (str-json->vector (slurp file-json)))
+
+(defn filter-rdfs
+  "Keeps only the rdf.yaml from a seq of paths"
+  [paths]
+  (filter #(= (fs/file-name %) "rdf.yaml") paths))
+
+(defn resource->paths
+  "Takes a resource/version to test and returns its path(s) (multiple if globbing)"
+  [root resource-map]
+  (let [{:keys [resource_id version_id]} resource-map]
+    (cond
+      (= resource_id "**") (filter-rdfs (fs/glob root "**"))
+      (= version_id "**") (filter-rdfs (fs/glob (fs/path root resource_id) "**"))
+      :else (seq (fs/path root resource_id version_id "rdf.yaml")))))
+
+
 
 (defn -main
-  "-s input is a raw json string, -j input is a file.json"
+  "-s input is a raw json string, -j input is path to a file.json
+  Invoke with bb -m collection <args> in the java_CI_scripts working directory"
   [& args]
   (println *command-line-args*)
   (println args)
--- java_CI_scripts/test/collection_test.clj
+++ java_CI_scripts/test/collection_test.clj
@@ -1,14 +1,53 @@
 (ns collection-test
-  (:require [clojure.test :refer [deftest is testing use-fixtures]])
-  (:require [collection :refer :all]))
+  (:require [clojure.test :refer [deftest is testing use-fixtures run-tests run-test]]
+            [collection :refer :all]
+            [babashka.fs :as fs]))
+
+(deftest collection-root-test
+  (is (= (fs/file-name COLLECTION-ROOT) "rdfs"))
+  (is (= (mapv fs/file-name (fs/list-dir COLLECTION-ROOT))
+         ["10.5281" "bioimageio" "deepimagej" "fiji" "hpa" "ilastik" "imjoy" "zero"])
+      "This test may fail if new partners/zenodo dois are added to the zoo"))
+
+(deftest str-json->vector-test
+  (testing "Input is a raw json string"
+    (let [str-json "{\"include\": [{\"resource_id\": \"**\", \"version_id\": \"**\"}]}"
+          parsed (str-json->vector str-json)]
+      (is (= (count parsed)) 1)
+      (is (= (:resource_id (nth (str-json->vector str-json) 0)) "**")))))
+
+(deftest file-json->vector-test
+ (testing "Input is a json file (java.io.File)"
+   (let [parsed (file-json->vector (fs/file "pending_matrix/two_models.json"))
+         [m1 _] parsed
+         {:keys [resource_id version_id]} m1]
+     (is (= (count parsed) 2))
+     (is (= resource_id "10.5281/zenodo.7261974"))
+     (is (= version_id "7261975"))))
+ (testing "Input is a string that is a path to a json file"
+   (let [parsed (file-json->vector "./pending_matrix/two_models.json")
+         [_ m2] parsed
+         {:keys [resource_id version_id]} m2]
+     (is (= (count parsed) 2))
+     (is (= resource_id "deepimagej/DeepSTORMZeroCostDL4Mic"))
+     (is (= version_id "latest")))))
 
 (deftest -main-test
   (is (= '("a" "1" 2) (-main "a" "1" 2))))
+(run-tests 'collection-test)
 
-(deftest json->vector-test
-  (testing "Input is a raw json string"
-    (let [str-json "{\"include\": [{\"resource_id\": \"**\", \"version_id\": \"**\"}]}"
-          parsed (json->vector str-json)]
-      (is (= (count parsed)) 1)
-      (is (= (:resource_id (nth (json->vector str-json) 0)) "**"))))
-  )
+(deftest filter-rdfs-test
+  (let [all-rdfs (fs/glob COLLECTION-ROOT "**")
+        some-rdfs (fs/glob (fs/path COLLECTION-ROOT "10.5281/zenodo.5749843") "**")]
+    (is (= (count (filter-rdfs all-rdfs)) 164))
+    (is (= (count (filter-rdfs some-rdfs)) 2))))
+
+; TODO finish this
+(deftest resource->paths-test
+  (let [all-models (first (file-json->vector "pending_matrix/all_models.json"))
+        version-glob {:resource_id "10.5281/zenodo.5749843" :version_id "**"}
+        one-model (last (file-json->vector "pending_matrix/two_models.json"))]
+    (testing "globbing all resources and versions")
+    (testing "globbing all version of a resource")
+    (testing "paths of a single model (resource and version")
+    ))
