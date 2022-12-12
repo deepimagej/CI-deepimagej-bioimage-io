690fe25d6cee621fe41d01e09cba3b5720056160
ivan_winLap <ivan.ea7h@gmail.com>
2022-12-12 13:46:38 +0100

    parse json string

--- java_CI_scripts/src/collection.clj
+++ java_CI_scripts/src/collection.clj
@@ -1,9 +1,21 @@
 (ns collection
-  (:require [babashka.fs :as fs]))
+  (:require [babashka.fs :as fs]
+            [cheshire.core :as json]))
 
+(comment
+  (json/parse-string (slurp (fs/file "pending_matrix" "two_models.json")) true)
+  )
 ;(slurp "abc")
+(defn json->vector
+  "Returns the parsed list of resources/versions to test given a string json"
+  [str-json]
+  (let [parsed (json/parse-string str-json true)
+        {the-list :include} parsed]
+    the-list))
 
-(defn -main [& args]
+(defn -main
+  "-s input is a raw json string, -j input is a file.json"
+  [& args]
   (println *command-line-args*)
   (println args)
   args)
--- java_CI_scripts/test/collection_test.clj
+++ java_CI_scripts/test/collection_test.clj
@@ -4,3 +4,11 @@
 
 (deftest -main-test
   (is (= '("a" "1" 2) (-main "a" "1" 2))))
+
+(deftest json->vector-test
+  (testing "Input is a raw json string"
+    (let [str-json "{\"include\": [{\"resource_id\": \"**\", \"version_id\": \"**\"}]}"
+          parsed (json->vector str-json)]
+      (is (= (count parsed)) 1)
+      (is (= (:resource_id (nth (json->vector str-json) 0)) "**"))))
+  )
