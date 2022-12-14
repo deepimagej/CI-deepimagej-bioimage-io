d425be3cf7b5b90d4f8d75c1768c82e4de7b04e5
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2022-12-14 10:32:45 +0100

    started test_summaries

---
+++ java_CI_scripts/test/test_summaries_test.clj
@@ -0,0 +1,7 @@
+(ns test-summaries-test
+  (:require [clojure.test :refer [deftest is testing]])
+  (:require [test-summaries :refer :all]))
+
+; create folder, check if correct, then delete it!
+(deftest create-test-sum-dir-test
+  )
