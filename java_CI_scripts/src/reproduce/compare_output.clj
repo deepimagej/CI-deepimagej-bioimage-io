0424bb81041d0a91ee49d48d3bd910a495befbf6
ivan_winLap <ivan.ea7h@gmail.com>
2023-01-03 21:30:22 +0100

    finish test one with dij

--- java_CI_scripts/src/reproduce/compare_output.clj
+++ java_CI_scripts/src/reproduce/compare_output.clj
@@ -1,5 +1,38 @@
-(ns reproduce.compare-output)
+(ns reproduce.compare-output
+  (:require [clojure [edn :as edn] [test :refer :all]]
+            [clojure.java [io :refer [file]]]))
 ;this is also run in fiji
 
-;communicate the results (edn)to write tests summaries
-;save in model-folder (in the comm-file
+;communicate the results in a file (edn) to write tests summaries
+; saved in model-folder (in the comm-file)
+; output does not exists => dij run failed
+; output exists
+; - it is not equal to expected output => failed
+; - it is equal to expected output => pass
+
+(def comm-file (file (System/getProperty "user.home") "models_to_test.edn"))
+(def all-models "All Models in a vector, from the comm-file" (edn/read-string (slurp comm-file)))
+(def fiji-home (file (System/getProperty "user.home") "blank_fiji" ))
+(def model-dir "the_model")
+(def output-name "CI_OUTPUT.tif")
+
+(defn output-created?
+  "Predicate says if the output was created from the deepimagej run in specified folder"
+  [folder]
+  (->> (file folder)
+       ; file-seq ; is recursive (!! aka slow for $HOME !!)
+       (.listFiles)
+       (map #(.getName %))
+       (filter #(= % output-name))
+       (empty?)
+       not))
+
+;; --
+;; -- Tests in the same file, as I couldn't find a way to require own ns's in a fiji script
+;; --
+
+(deftest output-created?-test
+         (is (not (output-created? (.getParent comm-file))))
+         (is (output-created? (:model-folder (first all-models)))))
+
+(println (run-tests))
--- java_CI_scripts/src/reproduce/test_many_with_deepimagej.clj
+++ java_CI_scripts/src/reproduce/test_many_with_deepimagej.clj
@@ -1,40 +1,56 @@
 (ns reproduce.test-many-with-deepimagej
   (:require [clojure [edn :as edn] [test :refer :all] [pprint :as ppr]]
-            [clojure.java [io :refer [file]]])
-  ;(:import [ij IJ])
+            [clojure.java [io :refer [file make-parents copy delete-file]]])
+  (:import [ij IJ])
   )
 
 (def comm-file (file (System/getProperty "user.home") "models_to_test.edn"))
-(def all-models "All Models in a vector, from the comm-file"
-  (edn/read-string (slurp comm-file)))
-
+(def all-models "All Models in a vector, from the comm-file" (edn/read-string (slurp comm-file)))
+(def fiji-home (file (System/getProperty "user.home") "blank_fiji" ))
+(def model-dir "the_model")
 (def output-name "CI_OUTPUT.tif")
 
-(defn output-created?
-  "Predicate says if the output was created from the deepimagej run in specified folder"
-  [folder]
-  (->> (file folder)
-       ; file-seq ; is recursive (!! aka slow)
-       (.listFiles)
-       (map #(.getName %))
-       (filter #(= % output-name))
-       (empty?)
-       not))
+(defn copy-file
+  [in-dir out-dir filename]
+  (copy (file in-dir filename) (file out-dir filename)))
+
+(defn copy-model-folder
+  "Copy each file in the model folder to Fiji.app/models"
+  ([fiji-home model-folder]
+   (let [in-file (file model-folder model-dir)
+         out-file (file fiji-home "Fiji.app" "models" model-dir )
+         ls (seq (.listFiles in-file))]
+     (do (make-parents (file out-file "a_file.txt"))
+         (mapv #(copy-file in-file out-file (.getName %)) ls))))
+  ([dij-model] (copy-model-folder (System/getProperty "user.home") dij-model)))
+
+(defn delete-model-folder
+  "Delete the model from Fiji.app/models (once inference is finished)"
+  ([fiji-home]
+   (let [m-dir (file fiji-home "Fiji.app" "models" model-dir)]
+     (doseq [f (reverse (file-seq m-dir))]
+       (delete-file f))))
+  ([] (delete-model-folder (System/getProperty "user.home"))))
 
 (defn test-one-with-deepimagej
+  "Test one model with deepimagej"
+  [{:keys [dij-arg model-folder input-img]}]
+  (let [imp (IJ/openImage (str model-folder model-dir "/" input-img))]
+    (copy-model-folder fiji-home model-folder)
+    (try (IJ/run imp "DeepImageJ Run" dij-arg)
+         (catch Exception e (println "-- Error during deepimagej run")))
+    (IJ/saveAs "Tiff" (str model-folder output-name))
+    (delete-model-folder fiji-home)))
+
+(defn test-one-with-info
   "Wrapper for testing a model, adding time and print information"
   [{:keys [name nickname]}])
 
-(defn test-one
-  "Test one model with deepimagej"
-  [{:keys [dij-arg model-folder input-img]}]
-  (let [imp nil ]))
-
 (defn -main [& args]
   (println "Hi from main, the args are:" args)
-  (println "The clojure version is: " *clojure-version*))
+  (println "The clojure version is: " *clojure-version*)
+  (test-one-with-deepimagej (first all-models)))
 
-(-main "a" 1 2)
 
 ;; --
 ;; -- Tests in the same file, as I couldn't find a way to require own ns's in a fiji script
@@ -46,8 +62,7 @@
     (print (format "There are %d models in %s.\n The first one is:" (count all-models) comm-file))
     (ppr/pprint (first all-models))))
 
-(deftest output-created?-test
-  (is (not (output-created? (.getParent comm-file))))
-  (is (output-created? (:model-folder (first all-models)))))
-
 (println (run-tests))
+
+
+(-main "a" 1 "2")
