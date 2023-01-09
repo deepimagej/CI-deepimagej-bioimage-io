938b8f5f7618a9aaf5d2f332e02b258f3abac71b
ivan_winLap <ivan.ea7h@gmail.com>
2023-01-09 10:41:26 +0100

    download and write to file

--- java_CI_scripts/src/downloads/download.clj
+++ java_CI_scripts/src/downloads/download.clj
@@ -1,5 +1,6 @@
 (ns downloads.download
-  (:require [babashka [curl :as curl] [fs :as fs]]))
+  (:require [clojure.java.io :refer [as-url copy]]
+            [babashka [curl :as curl] [fs :as fs]]))
 
 (defmacro my-time
   "Variation on clojure.core/time: https://github.com/clojure/clojure/blob/clojure-1.10.1/src/clj/clojure/core.clj#L3884
@@ -12,26 +13,29 @@
          duration# (java.time.Duration/between start# end#)]
      (hash-map :duration duration# :iso (str duration#)  :return ret#)))
 
-(defn get-url->byte-arr
-  "Gets the contents of a url as a byte array"
-  [url]
+(defn get-url-response
+  "Gets the response of a url as a hash map"
+  [^String url]
   (let [basic-opts {:as :bytes :throw false},
         opts (if (= "Windows 10" (System/getProperty "os.name"))
                (assoc basic-opts :compressed false)
                basic-opts)]
     (curl/get url opts)))
 
-(defn byte-arr->file
-  "Save a byte-array as a file"
+(defn get-url-filename
+  "Gets the filename from a url"
+  [^String url]
+  (->> (as-url url) (.getPath) fs/path fs/file-name))
+
+(defn byte-arr->file!
+  "Save a byte-array as a file. Ref source https://gist.github.com/philippkueng/11377226"
   [b-arr dir file-name]
-  (spit b-arr (fs/file dir file-name)))
+  (copy b-arr (fs/file dir file-name)))
+
+
 
 (defn download-weights
   [model-record])
 
 (defn download-images
       [model-record])
-
-(comment
-  "use (some->) for robust pipeline. source https://gist.github.com/philippkueng/11377226"
-  )
--- java_CI_scripts/test/downloads/download_test.clj
+++ java_CI_scripts/test/downloads/download_test.clj
@@ -1,7 +1,8 @@
 (ns downloads.download-test
   (:require [downloads.download :refer :all]
             [test-setup :refer [load-test-paths load-model-records an-edn model-records]]
-            [clojure [test :refer :all] [edn :as edn]]))
+            [clojure [test :refer :all] [edn :as edn]]
+            [babashka.fs :as fs]))
 
 (use-fixtures :once load-test-paths load-model-records)
 
@@ -14,7 +15,7 @@
     (is (<= (.getNano (:duration time-map)) 1e9) "time taken is 1s and a little bit more (not 2s)")
     (is (= [iso-str "1" "S"] (re-matches #"PT(\d).+\d+(S)" iso-str)))))
 
-(deftest get-url->byte-arr-test
+(deftest get-url-response-test
   (let [url-1 "https://yaml.org/"
         ;ulr- (:source (first (:weights (first @model-records)))) ; 5Mb too big for fast tests
         url-2 "https://zenodo.org/does-not-exist"
@@ -22,28 +23,51 @@
         url-4 33
         url-5 (:url an-edn)]
     (testing "A valid url"
-      (let [response (get-url->byte-arr url-1)]
+      (let [response (get-url-response url-1)]
         (is (= 200 (:status response)))
         (is (= "" (:err response)))
         (is (zero? (:exit response)))
         (is (= 19586 (alength (:body response))) "This will fail if yaml.org web is changed")))
     (testing "An url that does not exist"
-      (let [response (get-url->byte-arr url-2)]
+      (let [response (get-url-response url-2)]
         (is (= 404 (:status response)))
         (is (= "" (:err response)))
         (is (> (alength (:body response)) 0))))
     (testing "host does not exist"
-      (let [response (get-url->byte-arr url-3)]
+      (let [response (get-url-response url-3)]
         (is (nil? (:status response)))
         (is (> (count (:err response)) 0))
         (is (= 6 (:exit response)))
         (is (zero? (alength (:body response))))))
     (testing "type of arg is not string"
-      (let [response (get-url->byte-arr url-4)]
+      (let [response (get-url-response url-4)]
         (is (zero? (alength (:body response))))
         (is (= 3 (:exit response)))))
     (testing "An edn file from github"
-      (let [body (:body (get-url->byte-arr url-5))
+      (let [body (:body (get-url-response url-5))
             parsed (edn/read-string (slurp body))]
         (is (= 145 (alength body)))
         (is (= parsed (dissoc an-edn :url)))))))
+
+(deftest get-url-filename-test
+  (is (= "an.edn" (get-url-filename (:url an-edn))))
+  (is (= "confocal_pnas_2d.pytorch"
+         (get-url-filename (:source (first (:weights (first @model-records))))))))
+
+(deftest byte-arr->file!-test
+  (let [arrays {:ba-1 (:body (get-url-response (:url an-edn)))
+                :ba-2 (.getBytes "abcd")
+                ; download an image
+                :ba-3 (:body (get-url-response "https://www.uc3m.es/ss/Satellite?blobcol=urldata&blobkey=id&blobtable=MungoBlobs&blobwhere=1371552339304&ssbinary=true"))}
+        toy-dir (fs/path "out" "test" "toy_folder")
+        gen-filename (fn [kwd] (str (name kwd) ".file"))]
+    ; create the parent folder if it doesn't exist
+    (fs/create-dirs toy-dir)
+    ; write byte arrays in files
+    (doall (map (fn [[k v]] (byte-arr->file! v toy-dir (gen-filename k))) arrays))
+    ; testing
+    (is (= (edn/read-string (slurp (fs/file toy-dir (gen-filename :ba-1))))
+           (dissoc an-edn :url)))
+    (is (= "abcd" (slurp (fs/file toy-dir (gen-filename :ba-2)))))
+    (is (= 4938 (.length (fs/file toy-dir (gen-filename :ba-3)))))
+    ))
