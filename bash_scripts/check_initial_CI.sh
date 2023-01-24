409cd6671d218000d9941677f54c9aeadacf6bbc
ivan_winLap <ivan.ea7h@gmail.com>
2023-01-24 11:24:50 +0100

    check models are populated

--- bash_scripts/check_initial_CI.sh
+++ bash_scripts/check_initial_CI.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 tree -h --du --dirsfirst -L 3 test_summaries/ # test-summaries folder should exist
-tree -h --du --dirsfirst -L 3 models/ # models folder should exist
+tree -h --du --dirsfirst -L 5 models/ # models folder should exist and be populated
 echo -- Print contents of test summaries
 find test_summaries/ -name '*.yaml' -exec head -n 1 {} +
 echo -- Print contents of comm file
