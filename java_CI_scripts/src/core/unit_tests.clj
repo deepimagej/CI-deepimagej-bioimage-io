9b09ba40ccb7d2dee10698fc3720467adb5785be
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2023-01-26 11:58:09 +0100

    refactor on all unit tests symbols

--- java_CI_scripts/src/core/unit_tests.clj
+++ java_CI_scripts/src/core/unit_tests.clj
@@ -1,21 +1,18 @@
 (ns core.unit-tests
   "Require and run all tests for this project"
-  (:require collection-test
-            models-test
-            [summaries summary-test]
-            [downloads initial-checks-test download-test p-process-test]
-            [reproduce communicate-test run-fiji-scripts-test]
-            [core cli-test]
-            [clojure.test :refer [run-tests]]))
+  (:require [clojure.test :refer [run-tests]]))
 
-; todo imporvement: run tests on all symbols required (to not type twice)
-; (apply require ['collection-test 'models-test])
-; (apply run-tests ['collection-test 'models-test])
+(def nss-to-test
+  "Names of the namespaces to test"
+  ['collection-test
+   'models-test
+   'summaries.summary-test
+   'downloads.initial-checks-test 'downloads.download-test 'downloads.p-process-test
+   'reproduce.communicate-test 'reproduce.run-fiji-scripts-test
+   'core.cli-test])
+
+; All the namespaces need first to be required
+(apply require nss-to-test)
 
 (defn run-all-tests []
-  (run-tests 'collection-test
-             'models-test
-             'summaries.summary-test
-             'downloads.initial-checks-test 'downloads.download-test 'downloads.p-process-test
-             'reproduce.communicate-test 'reproduce.run-fiji-scripts-test
-             'core.cli-test))
+  (apply run-tests nss-to-test))
