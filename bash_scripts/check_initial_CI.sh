2f939b0bad8a10975336ab27b7dad81c9d6e0422
ivan_deck <ivan.e7a@gmail.com>
2023-01-30 23:40:28 +0100

    workflows for complete CI pipeline

--- bash_scripts/check_initial_CI.sh
+++ bash_scripts/check_initial_CI.sh
@@ -5,4 +5,4 @@ tree -h --du --dirsfirst -L 5 models/ # models folder should exist and be popula
 echo -- Print contents of test summaries
 find test_summaries/ -name '*.yaml' -exec head -n 1 {} +
 echo -- Print contents of comm file
-head -n 30 ~/models_to_test.edn 
+head -n 10 resources/models_to_test.txt
