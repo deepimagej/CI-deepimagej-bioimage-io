f526ee7d2a060b2cae0da7d7f74a8a7bdf074ea6
ivan_deck <ivan.e7a@gmail.com>
2023-02-17 22:30:15 +0100

    more functions to utils

--- java_CI_scripts/src/utils.clj
+++ java_CI_scripts/src/utils.clj
@@ -1,6 +1,19 @@
 (ns utils
-  "Generic functions")
+  "Generic functions"
+  (:require [clojure.java.io :as io]))
 
 (defn count-dict
   "Counts dictionary entries which are seqs"
   [d] (reduce (fn [m [k v]] (assoc m k (count v))) {}  d))
+
+(defn read-lines
+  "Reads every line on a file, returns a vector of strings"
+  [file]
+  (with-open [rdr (io/reader file)]
+    (into [] (line-seq rdr))))
+
+(defn local-time
+  "Returns local date and time"
+  ([] (local-time "Europe/Paris"))
+  ([zone-str]
+   (str (java.time.LocalDateTime/now (java.time.ZoneId/of zone-str)))))
