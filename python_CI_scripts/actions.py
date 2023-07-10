1127126cd2c4b79d031709220e887a4b11a629e3
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2023-07-10 11:08:01 +0200

    fix summaries bug

--- python_CI_scripts/actions.py
+++ python_CI_scripts/actions.py
@@ -11,15 +11,17 @@ def initial_pipeline(ini_return, input_json):
     rdf_paths = collection.get_rdfs_to_test(input_json)
     model_records = list(filter(lambda x: errors.is_model(x), (map(lambda x: models.build_model(x), rdf_paths))))
     models_discriminated = errors.separate_by_error(model_records, errors.init_errors_fns)
+    keep_testing = models_discriminated["keep-testing"]
 
     # Reset contents of test_summaries/Readme.md
     with open(FILES["summa-readme"], "w") as f:
         f.write(CONSTANTS["summa-readme-header"] + "\n")
 
-    summaries.write_summaries_from_error(models_discriminated["error-found"])
+    list(map(lambda x: summaries.write_summaries_from_error(x), models_discriminated["error-found"].items()))
 
+    print("{} models to keep testing".format(len(keep_testing)))
     if ini_return:
-        return models_discriminated["keep-testing"]
+        return keep_testing
 
 
 def download_pipeline(input_json):
