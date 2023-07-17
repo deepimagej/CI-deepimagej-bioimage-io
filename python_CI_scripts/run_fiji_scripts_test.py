1f16bdb2efb97d1b2a18a4f1c03d20985ef9ce16
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2023-07-17 14:29:39 +0200

    testing run_fiji_scripts

--- python_CI_scripts/run_fiji_scripts_test.py
+++ python_CI_scripts/run_fiji_scripts_test.py
@@ -3,12 +3,11 @@ import datetime
 from run_fiji_scripts import *
 import communicate as comm
 
-from models_test import a_model_record, pt_model_record, tf_model_record
+from models_test import pt_model_record, tf_model_record
 from communicate_test import affable_sark_model, joyful_deer_model, creative_panda_model
 
-model_paths = list(map(lambda x: comm.get_model_folder_str(x), [a_model_record, pt_model_record, tf_model_record,
-                                                                affable_sark_model, joyful_deer_model,
-                                                                creative_panda_model]))
+model_paths = list(map(lambda x: comm.get_model_folder_str(x), [pt_model_record, tf_model_record, affable_sark_model,
+                                                                joyful_deer_model, creative_panda_model]))
 
 # not so many assertions, outputs depend on absolute path of computer where tests are running
 
@@ -23,3 +22,5 @@ print(gen_messages(model_paths, k="start"))
 print(gen_messages(model_paths, k="end", t_ini=t))
 
 print("Testing quote_arg")
+assert quote_arg("abcv") == "folder='abcv'"
+print(quote_arg(model_paths[0]))
